b5daab1c222244996fe60d56830e89fa3eb13b49
elyseio <elyseio@proton.me>
2023-05-05 15:36:47 +0800

    added dynamically terminal prompt

--- js/index.js
+++ js/index.js
@@ -1,16 +1,77 @@
 const main = document.getElementById("main");
 const banner = document.getElementById("banner");
-const input = document.getElementById("input");
-input.value = "";
-input.focus();
 
 const help = [
     "whoami         - who made this",
     "whoareyou      - yes, who are you?",
     "projects       - view projects",
     "clear          - clear the terminal screen",
-    "secret         - I wonder what it does?"
+    "secret         - I wonder what it does?",
+    "help           - show list of valid commands"
 ];
+
+function userInput(input, div) {
+    input.addEventListener("keypress", (event) => {
+        if(event.key === "Enter" || event.keyCode === 13) {
+            const span = document.createElement("span");
+            const userInput = event.target.value;
+            span.innerHTML = userInput;
+            div.appendChild(span);
+            input.remove();
+            switch(userInput) {
+                case "whoami": 
+                    showWhoami();
+                    break;
+                case "whoareyou":
+                    showWhoareyou();
+                    break;
+                case "clear": 
+                    hideBanner();
+                    break;
+                case "help":
+                    showHelp();
+                    break;
+                default:
+                    error();
+                    break;
+            }
+        }
+    });
+}
+
+function createCLI() {
+    const div = document.createElement("div");
+    const span = document.createElement("span");
+    const newInput = document.createElement("input");
+    newInput.setAttribute("type", "text");
+    newInput.setAttribute("id", "input");
+    newInput.autofocus = true;
+    span.classList.add("whois");
+    span.innerHTML = "[intruder@term ~] $ ";
+    div.appendChild(span);
+    div.appendChild(newInput);
+    newInput.focus();
+    main.appendChild(div);
+    userInput(newInput, div);
+}
+
+function showWhoami() {
+    const p = document.createElement("p");
+    p.innerHTML = "Hey, I go by the name of elyseio!";
+    main.appendChild(p);
+    createCLI();
+}
+
+function showWhoareyou() {
+    const p = document.createElement("p");
+    p.innerHTML = "Do you intimately know yourself?";
+    main.appendChild(p);
+    createCLI();
+}
+
+function hideBanner() {
+    banner.classList.add("hide");
+}
 
 function showHelp() {
     const div = document.createElement("div");
@@ -20,32 +81,18 @@ function showHelp() {
         div.appendChild(p);
     });
     main.appendChild(div);
-}
-
-function hideBanner() {
-    banner.classList.add("hide");
+    createCLI();
 }
 
 function error() {
     const notFound = document.createElement("p");
     notFound.innerHTML = "Command not found. Type 'help' for the list of commands.";
     main.appendChild(notFound);
+    createCLI();
 }
 
-input.addEventListener("keypress", (event) => {
-    if(event.key === "Enter" || event.keyCode === 13) {
-        const userInput = event.target.value;
-        input.value = "";
-        switch(userInput) {
-            case "help":
-                showHelp();
-                break;
-            case "clear": 
-                hideBanner();
-                break;
-            default:
-                error();
-                break;
-        }
-    }
-});
+function start() {
+    createCLI();
+}
+
+start();
