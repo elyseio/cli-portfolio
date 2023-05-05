ea96afca751d50feb5f083d0d66c5ce924d86672
elyseio <elyseio@proton.me>
2023-05-05 12:45:59 +0800

    added input event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log("test");
+const input = document.getElementById("input");
+input.value = "";
+input.focus();
+
+const help = [
+    "whoami         - who made this",
+    "whoareyou      - yes, who are you?",
+    "projects       - view projects",
+    "clear          - clear the terminal screen",
+    "secret         - I wonder what it does?"
+];
+
+input.addEventListener("keypress", (event) => {
+    if(event.key === "Enter") {
+        console.log(event.target.value);
+        input.value = "";
+    }
+});
